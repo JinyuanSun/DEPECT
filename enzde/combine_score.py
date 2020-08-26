808af5086f147065fe4173bf62112571a162f0f1
jinyuan sun <33344948+JinyuanSun@users.noreply.github.com>
2020-08-26 14:14:16 +0800

    Update combine_score.py

--- enzde/combine_score.py
+++ enzde/combine_score.py
@@ -5,7 +5,7 @@ adslist = []
 for line in ads:
     adslist.append(line.split()[-6:])
 
-depectcafile = open("depect_enzde_distance.sc")
+depectcafile = open("depect_enzde_geo.sc")
 with open("depect_enzde_combine.sc","w+") as newoutfile:
     i = 0
     for line in depectcafile:
